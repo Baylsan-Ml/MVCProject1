052e777efdb867bed2760b7ca492d4a21748ed9e
Baylsan-Ml <bailsanmletat99@gmail.com>
2026-06-13 16:32:01 +0300

    dashboard

---
+++ MVCProject1/areas/User/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MVCProject1.Data;
+using MVCProject1.Models;
+using System.Diagnostics;
+
+namespace MVCProject1.areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        public ApplicationDbContext context = new ApplicationDbContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.categories = context.Categories.ToList();
+            ViewBag.products = context.Products.ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
